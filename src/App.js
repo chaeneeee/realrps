807f9409c724e7d9e6911cc32783688602111e2b
chaeneeee <cheche0804@naver.com>
2024-03-21 01:32:40 +0900

    new

--- src/App.js
+++ src/App.js
@@ -23,35 +23,96 @@ const choice = {
 
 }
 
-function App() {
 
-  const [userSelect, setUserSelect] = useState(null)
-
-  const play = (userChoice) => {
-    setUserSelect(choice[userChoice])
+    function App() {
+      const [userSelect, setUserSelect] = useState(null);
+      const [computerSelect, setComputerSelect] = useState(null);
+      const [result, setResult] = useState("");
+      const play = (userChoice) => {
+        setUserSelect(choice[userChoice]);
+        let computerChoice = randomChoice();
+    setComputerSelect(computerChoice)
     // usersate 는 바로 userselect= choice(userchoice) 이렇게 넣어주면 x 
     // 다시 state를 바꿔주는 함수를 써줘야한다 set 으로 
-   
-  }
+    setResult(judgement(choice[userChoice], computerChoice));
+  };
+ 
+  
 
-  return (
-    <div>
-    <div className='main'>
-      <Box title="you" item={userSelect}/>
-      {/* <Box title="computer" /> */}
 
-    </div>
 
-    <div className='main'>
-      <button onClick={()=> play("scissors")}>가위</button>
-      <button onClick={()=> play("rock")}>바위</button>
-      <button onClick={()=> play("paper")}>보</button>
-    </div>
-    {/* play()  > 이게 함수를 실행시키는 형식이라 ui 를 부를 때 실행까지 시켜버린다 */}
-    {/* <button onClick={play("scissors")}>가위</button> 이렇게라면 */}
-    {/* 그래서 <button onClick={()=> play("scissors")}>가위</button> 이렇게 함수안에 넣어야한다 ui 그릴 때 실행되지 않도록  */}
-    </div>
-  );
+const randomChoice =() => {
+  // Object.keys -객체의 키값만 뽑아서 어레이로 만들어주는 함수다 ! 
+  // 지금 choice 의 키값은 rock scissors paper 로 0 1 2 가 만들어진다.
+  let itemArray = Object.keys(choice); 
+    console.log("item array", itemArray);
+
+  // math.random 은 0~1 사이의 랜덤한 숫자를 내보낸다.
+  // 그렇다면 가위바위보를 내보내는 것과 0~1 사이의 숫자를 내보내는 것을 어떻게 연관지을 것인가
+  // 배열로 한다 . 알고리즘 문제 ! 
+  let randomItem = Math.floor(Math.random() * itemArray.length);
+  console.log("random value", randomItem);
+  // 2.  몇  1. 몇 0. 몇 이렇게 된다 앞에 012 값만 가져오면 인덱스번호가 되므로 
+  //  소수점버리는 함수 Math.floor 을 쓴다.
+  let final = itemArray[randomItem];
+    return choice[final];
+  
 }
+const judgement = (user, computer) => {
+  console.log("user", user, "computer", computer);
+
+//user ===computer tie 비겼다 
+  //user ===rock , computer === scissors user 이긴 것 
+  //user === rock , computer === paper user 진 것 
+  // user === scissors computer paper user 이긴것 
+//  user === scissors computer rock user 진것
+// user === paper computer rock user 이긴것
+// user paper computer scissors user 진것 
+
+
+
+//  이걸 삼항 연산식으로 바꾸기 
+
+// if (user.name === computer.name ) {
+//   return "tie"
+// } else if  (user.name ==="Rock" ) {
+//     if (computer === "Scissors") {
+//       return "Win"}
+//       else {
+//         return "lose" }
+//       }
+//     }
+if (user.name == computer.name) {
+  return "tie";
+} else if (user.name == "Rock")
+  return computer.name == "Scissors" ? "win" : "lose";
+else if (user.name == "Scissors")
+  return computer.name == "Paper" ? "win" : "lose";
+else if (user.name == "Paper")
+  return computer.name == "Rock" ? "win" : "lose";
+};
+//  삼항연산식
+
+
+
+return (
+  <div>
+    <div className="main">
+      <Box title="You" item={userSelect} result={result} />
+      <Box title="Computer" item={computerSelect} result={result} />
+    </div>
+    <div className="main">
+      <button onClick={() => play("scissors")}>가위</button>
+      <button onClick={() => play("rock")}>바위</button>
+      <button onClick={() => play("paper")}>보</button>
+    </div>
+  </div>
+);
+}
+
+    /* play()  > 이게 함수를 실행시키는 형식이라 ui 를 부를 때 실행까지 시켜버린다 */
+    /* <button onClick={play("scissors")}>가위</button> 이렇게라면 */
+    /* 그래서 <button onClick={()=> play("scissors")}>가위</button> 이렇게 함수안에 넣어야한다 ui 그릴 때 실행되지 않도록  */
+
 
 export default App;
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,16 +1,28 @@
 import React from 'react'
 
 const Box = (props) => {
-    console.log("props" , props)
-  return (
-    <div className='box'>
-       <h1> {props.title} </h1>
-       <img className="item-img" src={props.item && props.item.img}/>
+
+  let result ;
+  if (
+     props.title === "Computer" && props.result !== "tie" && props.result !== ""  ) {
+
+ result = props.result === "win" ? "lose" : "win";
+  } else {// 위의 경우가 아니라면 props&nbsp;로 전달된 결과를 그대로 쓴다.
+    result = props.result;
+  }
+  
+ 
+return (
+
+    <div className={`box ${result}`}>
+      <h1>{props.title}</h1>
+      <h2>{props.item && props.item.name}</h2>
+      <img className="item-img" src={props.item && props.item.img} />
+
        {/* 맨처음에 값이 없을 수도 있다 그래서 에러나는 것을 막기위해 >> null 면 false 가 리턴된다. 앞에props.item 가 false 면 뒤에는 false 가 된다 
        &&연산자기 때문에 그래서 props.item 을 가드로 써준다    */}
-       <h2> WIN </h2>
-    </div>
-  )
-}
+ <h2>{result}</h2>
+    </div>  );
+};
 
 export default Box
